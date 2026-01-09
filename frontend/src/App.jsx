0d4905da4a3d8f2097c16b771e463260521ddc14
Kiran-F <fazaian2004@gmail.com>
2026-01-10 03:04:53 +0500

    project complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
@@ -8,19 +6,47 @@ import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 
-function App() {
-  return (
-    <>
-      <Navbar />
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from './store/UseAuthStore';
+import { useThemeStore } from './store/UseThemeStore';
 
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-      </Routes>
-    </>
+
+function App() {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({onlineUsers})
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+    if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+  return (
+      <div data-theme={theme}>
+        <Navbar/>
+
+        <Routes>
+          <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        </Routes>
+
+        <Toaster />
+      </div>
   )
 }
 
